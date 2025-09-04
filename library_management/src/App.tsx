438251f9fccf646ea2615e93b0ab98d4ab3f68b5
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-09-04 15:32:12 +0530

    feat: Routes added for admin and normal users

--- library_management/src/App.tsx
+++ library_management/src/App.tsx
@@ -2,14 +2,31 @@ import './App.scss'
 import Header from './components/Header'
 import BookList from './components/BookList'
 import BorrowedBookList from './components/BorrowedBookList'
+import AdminPanel from './components/AdminPanel'
+import { Routes, Route, Navigate, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className='library-main-container'>
       <Header />
       <div className='book-llist-container'>
-        <BookList />
-        <BorrowedBookList />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <BookList />
+                <BorrowedBookList />
+              </>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <AdminPanel />
+            }
+          />
+        </Routes>
       </div>
     </div>
   )
---
+++ library_management/src/components/AdminPanel.tsx
@@ -0,0 +1,7 @@
+const AdminPanel = () => {
+  return (
+    <div>Admin Panel</div>
+  )
+}
+
+export default AdminPanel
