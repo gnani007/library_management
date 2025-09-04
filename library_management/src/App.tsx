97ec5591a4a6df0b8fd0ce545dbd4f617eb97d82
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-09-04 13:36:35 +0530

    fix: remove unwanted code in the app and css files

--- library_management/src/App.tsx
+++ library_management/src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className='library-main-container'>
+      <h3>Library - Books Management</h3>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button>Login as User</button>
+        <button>Login as Admin</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='book-llist-container'>
+        <h4>Books</h4>
+        <div className='book-list'>
+          <div className='row'>
+            <div className='book-title'>Master in frontend</div>
+            <div className='author'>Alex Don</div>
+          </div>
+          <div className='row'>
+            <div className='book-title'>Master in frontend</div>
+            <div className='author'>Alex Don</div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
